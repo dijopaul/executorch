e9e754201e3dbd56c58c2a8635e3df41ac74c04f
Nishak <nishak@cadence.com>
2024-11-18 22:32:16 -0800

    Modify relu wrt new ref

--- backends/cadence/hifi/operators/quantized_relu_out.cpp
+++ backends/cadence/hifi/operators/quantized_relu_out.cpp
@@ -40,8 +40,8 @@ void quantized_relu_(
       -out_multiplier_data[0] * 1.0 / (1 << 31) * pow(2, out_shift_data[0]);
 
   for (size_t i = 0, e = input.numel(); i < e; ++i) {
-    const T temp = in[i] > q_zero_point ? (in[i] - q_zero_point) : 0;
-    out[i] = kernels::quantize<T>(temp, out_scale, out_zero_point);
+    float temp = in[i] > q_zero_point ? (in[i] - q_zero_point) : 0;
+    out[i] = kernels::quantize<T>(temp, out_scale, (int32_t)out_zero_point);
   }
 }
 
@@ -58,37 +58,24 @@ void quantized_relu_out(
     uint8_t* p_out = output.mutable_data_ptr<uint8_t>();
     uint8_t q_zero_point = in_zero_point.const_data_ptr<uint8_t>()[0];
 
-    WORD32 ret_val = xa_nn_vec_relu_asym8u_asym8u(
+    xa_nn_vec_relu_asym8u_asym8u(
         p_out,
         p_in,
         (int)q_zero_point,
         out_multiplier.const_data_ptr<int32_t>()[0],
         out_shift.const_data_ptr<int32_t>()[0],
         (int)out_zero_point,
-        0,
+        (int)out_zero_point,
         255,
         input.numel());
-
-    ET_CHECK_MSG(ret_val == 0, "An internal error occured");
-
   } else if (input.scalar_type() == executorch::aten::ScalarType::Char) {
-    const int8_t* p_in = input.const_data_ptr<int8_t>();
-    int8_t* p_out = output.mutable_data_ptr<int8_t>();
-    int8_t q_zero_point = in_zero_point.const_data_ptr<int8_t>()[0];
-
-    WORD32 ret_val = xa_nn_vec_relu_asym8s_asym8s(
-        p_out,
-        p_in,
-        (int)q_zero_point,
-        out_multiplier.const_data_ptr<int32_t>()[0],
-        out_shift.const_data_ptr<int32_t>()[0],
-        (int)out_zero_point,
-        -128,
-        127,
-        input.numel());
-
-    ET_CHECK_MSG(ret_val == 0, "An internal error occured");
-
+    quantized_relu_<int8_t>(
+        input,
+        in_zero_point,
+        out_zero_point,
+        out_multiplier,
+        out_shift,
+        output);
   } else {
     ET_CHECK_MSG(
         false,
