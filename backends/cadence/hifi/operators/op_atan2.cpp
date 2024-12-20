b5c45830ab93002cf090c9d5708e20721fa63185
Nishak <nishak@cadence.com>
2024-12-20 04:59:42 -0800

    adding ET_KERNEL_CHECK for allocate_temp_memory

--- backends/cadence/hifi/operators/op_atan2.cpp
+++ backends/cadence/hifi/operators/op_atan2.cpp
@@ -66,9 +66,14 @@ Tensor& atan2_out(
       WORD32* __restrict__ ptr1 =
           (WORD32* __restrict__)kernels::allocate_temp_memory(
               ctx, num_elm * sizeof(WORD32));
+
+      ET_KERNEL_CHECK(ctx, ptr1 != nullptr, MemoryAllocationFailed, out);
+
       WORD32* __restrict__ ptr2 =
           (WORD32* __restrict__)kernels::allocate_temp_memory(
               ctx, num_elm * sizeof(WORD32));
+
+      ET_KERNEL_CHECK(ctx, ptr2 != nullptr, MemoryAllocationFailed, out);
 
       WORD32* __restrict__ pin1 =
           (WORD32* __restrict__)a.const_data_ptr<float>();
@@ -108,8 +113,12 @@ Tensor& atan2_out(
           (FLOAT32* __restrict__)kernels::allocate_temp_memory(
               ctx, num_elm * sizeof(WORD32));
 
+      ET_KERNEL_CHECK(ctx, ptr1 != nullptr, MemoryAllocationFailed, out);
+
       FLOAT32* __restrict__ pin1 =
           (FLOAT32* __restrict__)a.const_data_ptr<float>();
+
+      ET_KERNEL_CHECK(ctx, pin1 != nullptr, MemoryAllocationFailed, out);
 
       WORD32 p_out_shape[kNnlibMaxDim];
       WORD32 p_inp1_shape[kNnlibMaxDim];
@@ -136,6 +145,8 @@ Tensor& atan2_out(
       WORD32* __restrict__ ptr1 =
           (WORD32* __restrict__)kernels::allocate_temp_memory(
               ctx, num_elm * sizeof(WORD32));
+
+      ET_KERNEL_CHECK(ctx, ptr1 != nullptr, MemoryAllocationFailed, out);
 
       WORD32* __restrict__ pin1 =
           (WORD32* __restrict__)b.const_data_ptr<float>();
