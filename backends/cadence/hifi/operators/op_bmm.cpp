c648477a0bdabbc71aad19787bd8417e557b3c7b
Nishak <nishak@cadence.com>
2025-01-30 22:34:16 -0800

    adding checks in bmm

--- backends/cadence/hifi/operators/op_bmm.cpp
+++ backends/cadence/hifi/operators/op_bmm.cpp
@@ -80,11 +80,16 @@ Tensor& bmm_out(
     WORD32* __restrict__ tmp =
           (WORD32* __restrict__)kernels::allocate_temp_memory(
               ctx, (batch_size * m * p) * sizeof(float));
+
+    ET_KERNEL_CHECK(ctx, tmp != nullptr, MemoryAllocationFailed, out);
+
     tmp[batch_size * m * p] = {0};
 
     WORD32* __restrict__ p_o =
           (WORD32* __restrict__)kernels::allocate_temp_memory(
               ctx, (batch_size * m * p) * sizeof(WORD32));
+
+    ET_KERNEL_CHECK(ctx, p_o != nullptr, MemoryAllocationFailed, out);
 
     for (int i = 0; i < batch_size; ++i) {
       const FLOAT32* __restrict__ p_mat1 = in_data + i * m * n;
